c9801e2b7d63bad8cd2002a79e7d2585c4bc9c31
sebaborjas <108552013+sebaborjas@users.noreply.github.com>
2022-07-27 17:55:40 -0300

    Primeros pasos con React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./App.css";
+import React, { Fragment } from "react";
+import Header from "./componentes/Header";
+import Formulario from "./componentes/Formulario";
+//LOS COMPONENTES SE UTILIZAN: < COMPONENTE />
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    //SI QUITO DIV DA ERROR PORQUE SE DEBE RETORNAR UN ELEMENTO QUE PUEDE CONTENER MUCHOS MAS
+    //SINO SE PUEDE IMPORTAR FRAGMENT
+    <Fragment>
+      <Header
+        titulo="Cotizador de Prestamos"
+        descripcion="Utiliza el formulario y obten una cotizacion"
+      />
+      <div className="container">
+        <Formulario />
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/componentes/Header.js
@@ -0,0 +1,5 @@
+import React, { Fragment } from "react";
+
+const Header = ({ titulo }) => <h1>{titulo}</h1>;
+
+export default Header;
